ba0025c591ee70bec44c0f0e1591153608eb8831
Дмитрий <dokuchaev.d@orionnet.ru>
2018-05-23 17:46:22 +0700

    cards

--- selles.php
+++ selles.php
@@ -8,8 +8,99 @@ include "login_via_cookie_or_session.php";
 include_once 'h&f/header.php';
 ?>
 
-
-
+<div class="container-fluid">
+    <div class="row" style="margin-top:2%;">
+        <div class="col-lg-3 col-md-6 col-sm-12">
+                        <div class="card border-primary" style="width: 18rem;">
+                  <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-outline-primary">Купить</a>
+                    <button class="btn btn-outline-success"><i class="fa fa-shopping-basket "></i></button>
+                  </div>
+                </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                    <div class="card" style="width: 18rem;">
+                  <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                        <div class="card" style="width: 18rem;">
+                  <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                  <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                  </div>
+                </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                            <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                            <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                            <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                            <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 my_card">
+                            <div class="card" style="width: 18rem;">
+                      <img class="card-img-top" src=".../100px180/" alt="Card image cap" width=150 height=150>
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                      </div>
+                    </div>
+        </div>
+    </div>
+    
+</div>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <hr/>
+        </div>
+    </div>
+</div>
 
 <?php
 require_once 'h&f/modules.php';
